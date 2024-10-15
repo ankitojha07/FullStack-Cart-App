4241a1387e70fc6cf028f9a097e64bdfb530e0e7
ANkit Ojha <ankitojha1409@gmail.com>
2024-10-15 23:20:35 +0530

    updated cors urls

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Frontend URL
+    origin: "https://cart-app-api.vercel.app/", // Frontend URL
     credentials: true, // Allow cookies (only if needed)
     methods: ["POST", "GET", "PUT"],
   })
